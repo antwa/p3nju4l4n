8c8b0255b2a8fe9313d53e92f5f2fc9734d074d1
indriyanto.net <indriyanto.net@gmail.com>
2012-10-12 03:15:51 +0000

    - perbaikan login

--- Penjualan/Form/Form Main/formLogin.vb
+++ Penjualan/Form/Form Main/formLogin.vb
@@ -12,11 +12,11 @@
 
             '# create query
             Db.FlushCache()
-            Db.Selects("PRO_USER.US_USERNAME, PRO_USER.USL_KDLEVEL, PRO_USER.US_NAMALENGKAP, PRO_USER.US_JABATAN, PRO_USER_LEVEL.USL_NAMA")
-            Db.From("PRO_USER")
-            Db.Join("PRO_USER_LEVEL", "PRO_USER.USL_KDLEVEL = PRO_USER_LEVEL.USL_KDLEVEL")
-            Db.Where("PRO_USER.US_USERNAME", txt_username.Text)
-            Db.Where("PRO_USER.US_PASSWORD", txt_password.Text, "=", "AND")
+            Db.Selects("a.username, a.kode_level, a.nama_lengkap, a.jabatan, b.nama_level")
+            Db.From("sys_user a")
+            Db.Join("sys_level b", "a.kode_level = b.kode_level")
+            Db.Where("a.username", txt_username.Text)
+            Db.Where("a.password", txt_password.Text, "=", "AND")
 
             ' put to datareader
             Dim dr As SqlClient.SqlDataReader
@@ -29,26 +29,26 @@
                 Auth = New cls_authentication
 
                 dr.Read()
-                Auth.Username = dr.Item("US_USERNAME").ToString
-                Auth.KdLevel = dr.Item("USL_KDLEVEL").ToString
-                Auth.Nama = dr.Item("US_NAMALENGKAP").ToString
-                Auth.Jabatan = dr.Item("US_JABATAN").ToString
-                Auth.NamaAkses = dr.Item("USL_NAMA").ToString
+                Auth.Username = dr.Item("username").ToString
+                Auth.KdLevel = dr.Item("kode_level").ToString
+                Auth.Nama = dr.Item("nama_lengkap").ToString
+                Auth.Jabatan = dr.Item("jabatan").ToString
+                Auth.NamaAkses = dr.Item("nama_level").ToString
 
                 '# ambil auth form
                 Db.FlushCache()
-                Db.Selects("PRO_USER.US_USERNAME, PRO_USER_LEVEL.USL_KDLEVEL, PRO_USER_MENU.USM_KDMENU, PRO_USER_MENU.USM_NAME, PRO_USER_MENU.USM_FORM, PRO_USER_LEVEL_DETAIL.USLD_A, PRO_USER_LEVEL_DETAIL.USLD_I, PRO_USER_LEVEL_DETAIL.USLD_U, PRO_USER_LEVEL_DETAIL.USLD_D, PRO_USER_LEVEL_DETAIL.USLD_V")
-                Db.From("PRO_USER")
-                Db.Join("PRO_USER_LEVEL", "PRO_USER.USL_KDLEVEL = PRO_USER_LEVEL.USL_KDLEVEL")
-                Db.Join("PRO_USER_LEVEL_DETAIL", "PRO_USER_LEVEL.USL_KDLEVEL = PRO_USER_LEVEL_DETAIL.USL_KDLEVEL")
-                Db.Join("PRO_USER_MENU", "PRO_USER_LEVEL_DETAIL.USM_KDMENU = PRO_USER_MENU.USM_KDMENU")
-                Db.Where("PRO_USER.US_USERNAME", txt_username.Text)
+                Db.Selects("a.username, b.kode_level, d.kode_menu, d.nama_menu, d.form, c._access, c._insert, c._update, c._delete, c._view")
+                Db.From("sys_user a")
+                Db.Join("sys_level b", "a.kode_level = b.kode_level")
+                Db.Join("sys_level_detail c", "b.kode_level = c.kode_level")
+                Db.Join("sys_menu d", "c.kode_menu = d.kode_menu")
+                Db.Where("a.username", txt_username.Text)
 
                 ' put to datareader
                 dr = Connection.ExecuteToDataReader(Db.GetQueryString)
 
                 While dr.Read
-                    Auth.addAuth(dr.Item("USM_KDMENU").ToString, dr.Item("USM_NAME").ToString, dr.Item("USM_FORM").ToString, CBool(dr.Item("USLD_A").ToString), CBool(dr.Item("USLD_I").ToString), CBool(dr.Item("USLD_U").ToString), CBool(dr.Item("USLD_D").ToString), CBool(dr.Item("USLD_V").ToString))
+                    Auth.addAuth(dr.Item("kode_menu").ToString, dr.Item("nama_menu").ToString, dr.Item("form").ToString, CBool(dr.Item("_access").ToString), CBool(dr.Item("_insert").ToString), CBool(dr.Item("_update").ToString), CBool(dr.Item("_delete").ToString), CBool(dr.Item("_view").ToString))
                 End While
 
                 formMDI.Show()
